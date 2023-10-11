7cc379f40b758266435d0ec9b94bb068331aeb78
David Martínez Tapia <dmmtapia.ux@gmail.com>
2023-10-11 11:26:24 -0300

    terminado

---
+++ my-react-app/src/Components/GeneratorColor.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const GeneradorDeCajas = () => {
+
+  const [colores, setColores] = useState([""]);
+  const [nombreColor, setNombreColor]= useState("");
+
+
+  const addColor = () => {
+    const coloresTemporal = [...colores];
+    coloresTemporal.push(nombreColor);
+    setColores(coloresTemporal)
+  }
+  
+
+  const handleColorChange = (e) => {
+    setNombreColor(e.target.value)
+  }
+
+
+  const renderColores = () => {
+    return colores.map((item, index) => (
+      <div key={index} style={{ 
+        width: "40px", 
+        height: "40px", 
+        background: item, 
+        margin: "10px" }}
+      ></div>
+    ));
+  };
+  
+
+  return (
+    <div className="colors-generator">
+
+      <div>
+          <input 
+            className="form-control mt-4" 
+            type="text" value={nombreColor} 
+            placeholder="Insert color"
+            onChange={handleColorChange} 
+          />
+      </div>
+
+      <div>
+          <button 
+            className="btn btn-primary mt-2" 
+            onClick={addColor}>Add Color</button>
+      </div>
+
+      <div className="colors-render">
+        {
+          renderColores()
+        }
+      </div>
+      
+      
+    </div>
+  );
+}
+ 
+export default GeneradorDeCajas;
+
